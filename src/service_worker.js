bda53e0c7ed23ef07ba6430f4195e5a4a21c90b0
Carlos Domínguez García <carlosdg97@gmail.com>
2018-07-30 11:12:50 +0100

    Fix: renamed dbhelper to utils in service worker

--- src/service_worker.js
+++ src/service_worker.js
@@ -10,7 +10,7 @@ self.addEventListener('install', event => {
                 'img/icons/icon16.png',
                 'css/styles.css',
                 'js/jake_archibald_idb.js',
-                'js/dbhelper.js',
+                'js/utils.js',
                 'js/main.js',
                 'js/restaurant_info.js',
                 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
